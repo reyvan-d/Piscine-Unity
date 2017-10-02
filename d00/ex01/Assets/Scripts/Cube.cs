64edaf18cf955b1229fa9f33f0687067fbb8a9d7
Reyno VAN DER WESTHUIZEN <reyvan-d@e4r24p2.wethinkcode.co.za>
2017-10-02 15:18:08 +0200

    d00/ex01

--- d00/ex01/Assets/Scripts/Cube.cs
+++ d00/ex01/Assets/Scripts/Cube.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour {
 
+	public float speed;
+	public char letter;
 	// Use this for initialization
 	void Start () {
-		
+		speed = Random.Range(0.1f, 0.3f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (letter == 'A') {
+			if (Input.GetKeyDown (KeyCode.A)) {
+				GameObject.Destroy (this.gameObject);
+			}
+		}
+
+		if (letter == 'S') {
+			if (Input.GetKeyDown (KeyCode.S)) {
+				GameObject.Destroy (this.gameObject);
+			}
+		}
+
+		if (letter == 'D') {
+			if (Input.GetKeyDown (KeyCode.D)) {
+				GameObject.Destroy (this.gameObject);
+			}
+		}
+		transform.localPosition += new Vector3 (0.0f, -speed, 0.0f);
+		if (transform.localPosition.y < -4.0f) {
+			GameObject.Destroy (this.gameObject);
+		}
 	}
 }
--- d00/ex01/Assets/Scripts/CubeSpawner.cs
+++ d00/ex01/Assets/Scripts/CubeSpawner.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour {
 
+	public GameObject A, S, D;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float spawnA = Random.Range (0.0f, 1.0f);
+		if (spawnA < 0.01f) {
+			GameObject.Instantiate (A);
+		}
+		float spawnS = Random.Range (0.0f, 1.0f);
+		if (spawnS < 0.01f) {
+			GameObject.Instantiate (S);
+		}
+		float spawnD = Random.Range (0.0f, 1.0f);
+		if (spawnD < 0.01f) {
+			GameObject.Instantiate (D);
+		}
 	}
 }
